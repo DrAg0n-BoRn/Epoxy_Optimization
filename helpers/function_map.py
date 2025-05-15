79faa7091926700cb0d413f67e58b4be94db264b
Karl <luigiloza@gmail.com>
2025-05-15 12:15:49 +0800

    First commit
    project start-o

---
+++ helpers/function_map.py
@@ -0,0 +1,16 @@
+import polars as pl
+
+
+def placeholder(column: pl.Series) -> pl.Series:
+    """
+    """
+    pass
+
+
+
+
+### Distribute rules to the columns
+function_map = {
+    'coating material': None,
+}
+
