178798395b27b38a9d65ab808266508a5c019942
Karl <luigiloza@gmail.com>
2025-06-05 19:18:59 +0800

    Update paths.py

--- paths.py
+++ paths.py
@@ -2,31 +2,27 @@ from pathlib import Path
 import os
 import sys
 
-# Set root directory
+# Set the root directory and add it to sys.path
 ROOT_DIR = Path(__file__).resolve().parent
-
-# Add the root directory to the system path
 if str(ROOT_DIR) not in sys.path:
     sys.path.append(str(ROOT_DIR))
 
-
 ### Directories ###
-RAW_DATA_DIR = os.path.join(ROOT_DIR, "raw_data")
 DATA_DIR = os.path.join(ROOT_DIR, "data")
 RESULTS_DIR = os.path.join(ROOT_DIR, "results")
 
-
 ### Files ###
-RAW_CSV_PATH = os.path.join(DATA_DIR, "start_data.csv")
-PROCESSED_CSV_PATH = os.path.join(DATA_DIR, "processed_data.csv")
 
 
 
+### Constants ###
+
+
+
+# Create directories
 def make_directories():
-    """
-    Creates directories if they do not exist
-    """
-    for d in [RAW_DATA_DIR, DATA_DIR, RESULTS_DIR]:
+    for d in [DATA_DIR,
+              RESULTS_DIR]:
         os.makedirs(d, exist_ok=True)
 
 
